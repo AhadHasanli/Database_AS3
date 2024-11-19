d2e2629502ca8de9173f9e4b529efa663dd995d6
Ahad <119402526+AhadHasanli@users.noreply.github.com>
2024-11-19 18:47:26 +0400

    1NF table is created

--- assignment3.sql
+++ assignment3.sql
@@ -20,3 +20,33 @@ COPY unnormalized_form (CRN, ISBN, Title, Authors, Edition, Publisher, Publisher
 FROM 'Unnormalized.csv'
 DELIMITER ';'
 CSV HEADER;
+
+
+
+-- Step 3: Transition from unnormalized_form to new table which is in 1NF, and insert datas from unnormalized_form
+CREATE TABLE firstNF_table (
+    CRN INTEGER,
+    ISBN BIGINT,
+    Title TEXT,
+    Author TEXT,
+    Edition INTEGER,
+    Publisher TEXT,
+    PublisherAddress TEXT,
+    Pages INTEGER,
+    Year INTEGER,
+    CourseName TEXT
+);
+
+INSERT INTO firstNF_table (CRN, ISBN, Title, Author, Edition, Publisher, PublisherAddress, Pages, Year, CourseName)
+SELECT 
+    CRN,
+    ISBN,
+    Title,
+	unnest(string_to_array(trim(both ' ' from REGEXP_REPLACE(Authors, '\s+', ' ', 'g')), ', ')) AS Author,
+    Edition,
+    Publisher,
+    PublisherAddress,
+    Pages,
+    Year,
+    CourseName
+FROM unnormalized_form;
