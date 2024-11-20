60d1eed2c04c0f8f90861ba6c41addb5647f721b
Ahad <119402526+AhadHasanli@users.noreply.github.com>
2024-11-20 20:07:03 +0400

    3NF tables are created and datas inserted

--- assignment3.sql
+++ assignment3.sql
@@ -51,3 +51,81 @@ SELECT
     CourseName
 FROM unnormalized_form;
 
+-- Step 4: Transition from 1NF to new table which is in 2NF, and insert datas from 1NF table
+CREATE TABLE course_details (
+    CRN INTEGER PRIMARY KEY,
+    CourseName TEXT
+);
+
+INSERT INTO course_details (CRN, CourseName)
+SELECT DISTINCT CRN, CourseName
+FROM firstNF_table;
+
+
+
+CREATE TABLE books (
+    ISBN BIGINT PRIMARY KEY,
+    Title TEXT,
+    Edition INTEGER,
+    Publisher TEXT,
+    PublisherAddress TEXT,
+    Pages INTEGER,
+    Year INTEGER
+);
+
+INSERT INTO books (ISBN, Title, Edition, Publisher, PublisherAddress, Pages, Year)
+SELECT DISTINCT ISBN, Title, Edition, Publisher, PublisherAddress, Pages, Year
+FROM firstNF_table;
+
+
+
+CREATE TABLE author_details (
+    ISBN BIGINT,
+    Author TEXT,
+    FOREIGN KEY (ISBN) REFERENCES books(ISBN)
+);
+
+INSERT INTO author_details (ISBN, Author)
+SELECT DISTINCT ISBN, Author
+FROM firstNF_table;
+
+
+
+CREATE TABLE coursebook (
+    CRN INTEGER,
+    ISBN BIGINT,
+    PRIMARY KEY (CRN, ISBN),
+    FOREIGN KEY (CRN) REFERENCES course_details(CRN),
+    FOREIGN KEY (ISBN) REFERENCES books(ISBN)
+);
+
+INSERT INTO coursebook (CRN, ISBN)
+SELECT DISTINCT CRN, ISBN
+FROM firstNF_table;
+
+
+-- Step 5: Transition from 2NF to new table which is in 3NF, and insert datas from 1NF table
+CREATE TABLE publishers (
+    Publisher TEXT PRIMARY KEY,
+    PublisherAddress TEXT
+);
+
+INSERT INTO publishers (Publisher, PublisherAddress)
+SELECT DISTINCT Publisher, PublisherAddress
+FROM books;
+
+
+
+CREATE TABLE books_ThirdNF (
+    ISBN BIGINT PRIMARY KEY,
+    Title TEXT,
+    Edition INTEGER,
+    Publisher TEXT,
+    Pages INTEGER,
+    Year INTEGER,
+    FOREIGN KEY (Publisher) REFERENCES publishers(Publisher)
+);
+
+INSERT INTO books_ThirdNF (ISBN, Title, Edition, Publisher, Pages, Year)
+SELECT DISTINCT ISBN, Title, Edition, Publisher, Pages, Year
+FROM books;
